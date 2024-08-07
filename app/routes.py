31d00783128b3742b96a58ee95ad01ed8343ec1e
christofranz <franzke.christoph@web.de>
2024-08-07 20:47:24 +0200

    Use tokens and jwt for requests

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,11 @@ from flask import request, jsonify
 from app import app, mongo
 from app.models import User
 import jwt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import datetime
 
 SECRET_KEY = app.config['SECRET_KEY']
+jwt = JWTManager(app)
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -30,32 +32,27 @@ def login():
     if not user or not User.verify_password(user['password'], password):
         return jsonify({'message': 'Invalid credentials'}), 401
     
-    token = jwt.encode({'username': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, SECRET_KEY)
+    token = create_access_token(identity={'username': username, 'role': user['role']})
     return jsonify({'message': 'User logged in successfully', 'token': token, "role": user["role"]})
 
 @app.route('/protected', methods=['GET'])
+@jwt_required()
 def protected():
-    token = request.headers.get('Authorization')
-    if not token:
-        return jsonify({'message': 'Token is missing'}), 403
-    
-    try:
-        data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-    except:
-        return jsonify({'message': 'Token is invalid'}), 403
-    
-    return jsonify({'message': 'This is a protected route', 'user': data['username']})
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 @app.route('/set_role', methods=['POST'])
+@jwt_required()
 def set_role():
+
+    current_user = get_jwt_identity()
+    if current_user['role'] != 'admin':
+        return jsonify({"message": "Unauthorized - Admins only!"}), 403
+
     data = request.get_json()
-    admin_username = data['admin_username']
     target_username = data['target_username']
     new_role = data['new_role']
 
-    admin_user = mongo.db.users.find_one({"username": admin_username})
-    if admin_user and admin_user['role'] == 'admin':
-        mongo.db.users.update_one({"username": target_username}, {"$set": {"role": new_role}})
-        return jsonify({"message": "Role updated successfully"})
-    else:
-        return jsonify({"message": "Unauthorized"}), 403
+    mongo.db.users.update_one({"username": target_username}, {"$set": {"role": new_role}})
+    return jsonify({"message": "Role updated successfully"})
+    
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY') or 'you-will-never-guess'
     MONGO_URI = os.getenv('MONGO_URI') or 'mongodb://localhost:27017/mydb'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'fallback_secret_key')
     # Retrieve admin credentials
     admin_username = os.getenv('ADMIN_USERNAME')
     admin_password = os.getenv('ADMIN_PASSWORD')
